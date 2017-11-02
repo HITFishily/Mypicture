43f2bb5189a576023f55d004d153ab15eacf8d65
Huliya <HITFishily@users.noreply.github.com>
2017-11-02 20:33:31 +0800

    Update coedcherry.py

--- coedcherry.py
+++ coedcherry.py
@@ -1,7 +1,22 @@
 import re
 import urllib2  
 import time
-import random 
+import random  
+import os 
+import sys
+
+def make1sttxt(url):
+	user_agent = 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36'
+	headers = { 'User-Agent' : user_agent , 'Referer': 'https://www.coedcherry.com/models/sybil-a?page=1' }  
+	data = None
+	request = urllib2.Request(url, data, headers)
+	txt = urllib2.urlopen(request).read()
+	filename = 'origin.txt'
+	txtfile = open(filename,'wb')
+	txtfile.write(txt)
+	txtfile.close
+	print 'got the url'
+	return filename
 
 def get2ndpages(path):
 	file = open(path, 'r')
@@ -9,17 +24,18 @@ def get2ndpages(path):
 	list = re.findall(pattern,file.read())
 
 	finalurl = [''] * len(list)
+	print 'found ' + str(len(list)) + ' urls, ready to download pages'
 	count = 0
 	for i in list:
 		finalurl[count] = 'https://www.coedcherry.com' + i[18:-8] 
 		print finalurl[count]
 		count = count + 1
+	# os.remove(path)
 	return finalurl
-
 
 def make2ndtxt(finalurl):
 	count = 0
-	print 'start to download ' + str(len(finalurl)) + 'files:'
+	print 'start to download ' + str(len(finalurl)) + ' files:'
 	for i in finalurl:
 		user_agent = 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36'
 		headers = { 'User-Agent' : user_agent , 'Referer': 'https://www.coedcherry.com/models/sybil-a?page=1' }  
@@ -29,12 +45,12 @@ def make2ndtxt(finalurl):
 		txtfile = open(str(count) + '.txt','wb')
 		txtfile.write(d)
 		txtfile.close
-		print count
+		print 'downloaded ' + str(count) + ' files'
 		count = count + 1
 		time.sleep(random.randint(5, 10))
 	return len(finalurl)
 
-def makejpgurl(filepath):
+def makejpgurl(filepath):	
 	urlfile = open(filepath, 'r')
 	pattern = re.compile(r'src=".*jpg"')
 	reurl = re.findall(pattern, urlfile.read())
@@ -54,20 +70,31 @@ def makejpgurl(filepath):
 		count = count + 1
 	return finalurl
 
-def downloadpictures(finalurl, count):
+def downloadpictures(finalurl, count, num):
+	l = len(finalurl)
+	numbers = 0
 	for i in finalurl:
-		print i
 		user_agent = 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36'
 		headers = { 'User-Agent' : user_agent , 'Referer': 'https://www.coedcherry.com/models/sybil-a?page=1' }  
 		data = None
 		request = urllib2.Request(i, data, headers) 
 		d = urllib2.urlopen(request).read()
-		jpgfile = open(str(count) + '.jpg','wb')
+		jpgfile = open('result/' + str(count) + '_' + str(numbers) + '.jpg','wb')
 		jpgfile.write(d)
 		jpgfile.close
-		print count
-		count = count + 1
+		numbers = numbers + 1
+		print str(numbers) + '/' + str(l) + ' ' + str(count+1) + '/' + str(num)
 		time.sleep(random.randint(5, 10))
+print 'please paste the url you want to download: '
+xxx = raw_input()
 
-for i in range(make2ndtxt(get2ndpages('mmm.txt'))):
-	downloadpictures(makejpgurl(str(i) + '.txt'), 50*i)
+if os.path.exists('result') == False:
+	os.mkdir('result') 
+print 'start download!'
+num = make2ndtxt(get2ndpages(make1sttxt(xxx)))
+print 'start to download pictures:'
+for i in range(num):
+	downloadpictures(makejpgurl(str(i) + '.txt'), i, num)
+for i in range(num):
+	os.remove(str(i)+'.txt')
+print 'congratulations! download over!'
